92b7d7a15ed6b8ab51dde938f93c20d38ced6911
pascalheikamp <pascalheikamp17@gmail.com>
2024-01-10 13:06:52 +0100

    adding descripion in EventCards and creating modal-window for register

--- event-landvanons-app/src/components/EventCards/EventCards.jsx
+++ event-landvanons-app/src/components/EventCards/EventCards.jsx
@@ -39,7 +39,7 @@ function EventCards({data}) {
                         </div>
                     </div>
                     <div className="flex pb-5 p-5 mt-3 justify-around">
-                        <button><img src={bookmarkIcon}/></button>
+                        {/*<button><img src={bookmarkIcon}/></button>*/}
                         <button onClick={() => setShowModal(true)} className='bg-green-700 text-white'><h2>Inschrijven</h2></button>
                         {showModal ? (
                             <>
@@ -47,35 +47,12 @@ function EventCards({data}) {
                                     <div className="relative w-auto my-6 mx-auto max-w-3xl">
                                         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                                <h3 className="text-3xl font=semibold">General Info</h3>
-                                                <button
-                                                    className="bg-transparent border-0 text-black float-right"
-                                                    onClick={() => setShowModal(false)}
-                                                >
-                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
-                      x
-                    </span>
-                                                </button>
+                                                <h3 className="text-3xl font=semibold">Aangemeld voor evenement</h3>
                                             </div>
                                             <div className="relative p-6 flex-auto">
-                                                <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                                                    <label className="block text-black text-sm font-bold mb-1">
-                                                        First Name
-                                                    </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                    <label className="block text-black text-sm font-bold mb-1">
-                                                        Last Name
-                                                    </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                    <label className="block text-black text-sm font-bold mb-1">
-                                                        Address
-                                                    </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                    <label className="block text-black text-sm font-bold mb-1">
-                                                        City
-                                                    </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                </form>
+                                                <div className={"description-section"}>
+                                                    <p>Bedankt voor je aanmelding! <p>Wij sturen u zo spoedig mogelijk een bevestigingsmail.</p></p>
+                                                </div>
                                             </div>
                                             <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                                                 <button
@@ -85,13 +62,6 @@ function EventCards({data}) {
                                                 >
                                                     Close
                                                 </button>
-                                                <button
-                                                    className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                                                    type="button"
-                                                    onClick={() => setShowModal(false)}
-                                                >
-                                                    Submit
-                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -100,24 +70,12 @@ function EventCards({data}) {
                         ) : null}
                     </div>
                 </div>
-                <div className="detail-card w-40 shadow-2xl text-center mb-10 ">
+                <div className="detail-card w-48 shadow-2xl text-center relative mb-10 ">
                         <div key={1}>
                             <h2 className={"text-2xl mt-5 pb-5"}>Details</h2>
                             <h3 className="text-xl pb-3">Omschrijving:</h3>
-                            <div className={"description-section"}>
-                                <p className={"description"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Vestibulum vestibulum dapibus dui eu
-                                    maximus. Maecenas sed massa vehicula, tristique sapien vel, egestas tellus. Cras
-                                    vulputate et
-                                    neque ut blandit. Fusce suscipit ex felis, quis dapibus nulla vehicula eget. Vivamus
-                                    iaculis
-                                    sapien massa, at gravida lacus tempor et. Phasellus luctus justo eget augue bibendum
-                                    interdum.
-                                    Sed accumsan commodo sem, sed posuere lectus bibendum a. Donec nec laoreet purus.
-                                    Vestibulum
-                                    condimentum malesuada orci, in efficitur orci accumsan sed. Donec a maximus neque,
-                                    in interdum
-                                    nisl. Donec quis turpis mauris.</p>
+                            <div className={"w-full  pr-8 description-section"}>
+                                <p className={"description"}>{data.description}</p>
                             </div>
                         </div>
                     <div className={"button-section mt-3 flex justify-around"}>
--- event-landvanons-app/src/views/Register.jsx
+++ event-landvanons-app/src/views/Register.jsx
@@ -14,31 +14,34 @@ function Register() {
 
         // Add additional logic (e.g., sending data to a server, updating state, etc.)
     };
+    //
+    // const createAccount = async () => {
+    //     const response = fetch('http://127.0.0.1:8000/register', {
+    //         headers: {
+    //             'accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             "X-CSRF-Token": document.querySelector('input[name=_token]').value
+    //         },
+    //         method: 'POST',
+    //         body: JSON.stringify( {
+    //             name: name,
+    //             email: email,
+    //             password: password
+    //         })
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data)
+    //         }).catch((error) => (console.log(error)))
+    // }
 
-    const createAccount = async () => {
-        const response = fetch('http://127.0.0.1:8000/register', {
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/json',
-                "X-CSRF-Token": document.querySelector('input[name=_token]').value
-            },
-            method: 'POST',
-            body: JSON.stringify( {
-                name: name,
-                email: email,
-                password: password
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-            }).catch((error) => (console.log(error)))
-    }
+    const [showModal, setShowModal] = useState(false)
+
 
     return (
         <>
             <h1 className={"font-bold text-center text-2xl"}>Registreren</h1>
-            <form onSubmit={handleSubmit} className={"flex mt-10 justify-center"}>
+            <form onSubmit={handleSubmit} className={"flex mt-10  justify-center"}>
                 <div className={"mx-auto flex flex-col"}>
                     <span className={"flex pr-5 flex-row"}>
                     <label className={"font-semibold"}>Naam:</label>
@@ -48,19 +51,45 @@ function Register() {
                     <label className={"font-semibold"}>Email:</label>
                     <input value={email} onChange={(e)=> setEmail(e.target.value)} type={"text"} className={"border-grey-50 ml-5 mb-10 w-52 border-2 rounded"}/>
                            </span>
-                    <span className={"flex flex-row"}>
+                    <span className={"flex flex-"}>
                     <label className={"font-semibold"}>Wachtwoord:</label>
                     <input value={password} onChange={(e) => setPassword(e.target.value)} type={"password"} className={"border-grey-50 ml-5 mb-10 w-52 border-2 rounded"}/>
                            </span>
                     <span className={"flex flex-row"}>
                     <label className={"font-semibold"}>Wachtwoord bevestigen:</label>
-                    <input type={"text"} className={"border-grey-50 ml-5 mb-10 w-52 border-2 rounded"}/>
+                    <input type={"password"} className={"border-grey-50 ml-5 mb-10 w-52 border-2 rounded"}/>
                            </span>
-                    <input onClick={createAccount} type={"submit"} value={"Verzenden"}/>
+                    <input className={"register-button"} onClick={() => setShowModal(true)} type={"submit"} value={"Registreren"}/>
+                    {showModal ? (
+                        <>
+                            <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                        <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                                            <h3 className="text-3xl font=semibold">Registratie gelukt</h3>
+                                        </div>
+                                        <div className="relative p-6 flex-auto">
+                                            <div className={"description-section"}>
+                                                <p> <p>Het aanmaken van een account bij Land van ons is succesvol gelukt</p></p>
+                                            </div>
+                                        </div>
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                                                type="button"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Close
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    ) : null}
                 </div>
             </form>
         </>
     )
 }
-
 export default Register;
