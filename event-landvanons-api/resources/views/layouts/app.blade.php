d03dfd4c86b65957671445e8568f0c33acede5c3
Rayel2002 <1002893@hr.nl>
2024-01-17 16:52:28 +0100

    updated navbar

--- event-landvanons-api/resources/views/layouts/app.blade.php
+++ event-landvanons-api/resources/views/layouts/app.blade.php
@@ -18,55 +18,49 @@
 </head>
 <body>
 <div id="app">
-    <nav class="bg-custom-green p-2 mt-0 fixed w-full z-10 top-0">
-        <div class="container mx-auto flex flex-wrap items-center">
+    <nav class="bg-white p-2 mt-0 fixed w-full z-10 top-0">
+        <div class="elementor-scrolling-tracker elementor-scrolling-tracker-horizontal elementor-scrolling-tracker-alignment-">
+            <div class="current-progress" style="display: flex; width: 33.55%;">
+                <div class="current-progress-percentage" style="color: transparent;"></div>
+            </div>
+        </div>
+        <div class="container mx-auto flex items-center">
             <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
                 <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
-                    <span class="text-2xl pl-2"><i class="em em-grinning"></i> Land van ons</span>
+                    <img class="object-fit-contain h-32 w-auto" src="{{asset('/assets/lvo-logo.png')}}">
                 </a>
             </div>
-            <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
-
+            <div class="flex w-full items-center justify-between md:w-1/2 md:justify-end">
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto flex">
                     <!-- Authentication Links -->
                     @guest
                         @if (Route::has('login'))
-                            <li class="nav-item">
+                            <li class="nav-item p-6 text-white hover:shadow-xl py-2 px-4 rounded mb-2 border-b font-bold bg-lvo-purple">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                         @endif
 
                         @if (Route::has('register'))
-                            <li class="nav-item">
+                            <li class="nav-item p-6 mx-6 text-white hover:shadow-xl py-2 px-4 rounded mb-2 border-b font-bold bg-lvo-purple">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
+                        <li class="mx-6 bg-red-500 text-white hover:shadow-xl py-2 px-4 rounded mb-2 border-b font-bold" style="background-color: rgb(219, 51, 151)">{{ Auth::user()->name }}</li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="bg-red-500 text-white hover:shadow-xl py-2 px-4 rounded mb-2 border-b font-bold" style="background-color: rgb(219, 51, 151)">
+                                    Logout
+                                </button>
+                            </form>
                         </li>
                     @endguest
                 </ul>
             </div>
         </div>
     </nav>
-    <main class="py-4">
+    <main class="py-4 mt-32">
         @yield('content')
     </main>
 </div>
