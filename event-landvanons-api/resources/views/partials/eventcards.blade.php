b635e8b1ca4faec494892ed61f611a10e0e8667a
Rayel2002 <1002893@hr.nl>
2024-01-17 20:31:43 +0100

    deleted landvanons-app and fixed navigation

--- event-landvanons-api/resources/views/partials/eventcards.blade.php
+++ event-landvanons-api/resources/views/partials/eventcards.blade.php
@@ -51,7 +51,7 @@
                                 class=" closeButton text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
                                 type="button"
                             >
-                            Close
+                            Afsluiten
                             </button>
                         </div>
                     </div>
