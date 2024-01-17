b342eced98a8bf560de09f330cb18f343416106b
pascalheikamp <pascalheikamp17@gmail.com>
2024-01-17 14:28:08 +0100

    creating a middleware for visitor but it doenst work yet

--- event-landvanons-api/resources/views/partials/eventcards.blade.php
+++ event-landvanons-api/resources/views/partials/eventcards.blade.php
@@ -20,7 +20,10 @@
         </div>
         <div class="flex pb-5 p-5 mt-3 justify-around">
 {{--            {/*<button><img src={bookmarkIcon}/></button>*/}--}}
-            <button id="{{$event->id}}" class=' registerButton bg-green-700 text-white'><h2>Inschrijven</h2></button>
+            <form method="'post">
+                @csrf
+                <input type="submit"  value="Inschrijven" id="{{$event->id}}" class=' registerButton bg-green-700 text-white'></input>
+            </form>
             <div class="flex showcard justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                 <div class="relative w-auto my-6 mx-auto max-w-3xl">
                     <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -32,6 +35,16 @@
                                 <p>Bedankt voor je aanmelding! <p>Wij sturen u zo spoedig mogelijk een bevestigingsmail.</p></p>
                             </div>
                         </div>
+{{--                        @else--}}
+{{--                            <div class="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">--}}
+{{--                                <h3 class="text-3xl font=semibold">Aanmelden mislukt</h3>--}}
+{{--                            </div>--}}
+{{--                            <div class="relative p-6 flex-auto">--}}
+{{--                                <div class={"description-section"}>--}}
+{{--                                    <p>U moet zich eerst aanmelden voordat u kan deelnemen aan dit evenement</p></p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
                         <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                             <button
                                 id="closeButton"
